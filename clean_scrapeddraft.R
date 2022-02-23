e83dc915cb4b0a167d00240c86ab749e594378a5
Jonny Dalsey <jndalsey@gmail.com>
2022-02-23 15:05:23 -0800

    Cleaning scraped draft

---
+++ clean_scrapeddraft.R
@@ -0,0 +1,11 @@
+#################### clean_scrapedraft.R ####################
+
+##### libraries
+
+library(tidyverse)
+
+##### data
+
+# read in data created by final_scrapedraft.R
+scrapedPlayerDraft <- read.csv("ScrapedPlayerDraft.csv")
+
