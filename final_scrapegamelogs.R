291741cad11b668ad3b27f204d56c2adeef0e416
Jonny Dalsey <jndalsey@gmail.com>
2022-02-26 19:22:26 -0800

    Scrape the game logs

--- final_scrapegamelogs.R
+++ final_scrapegamelogs.R
@@ -1,6 +1,6 @@
 #################### final_scrapegamelogs.R ####################
 
-##### courses
+##### citations
 
 # Bill Petti's package
 # https://www.smartfantasybaseball.com/tag/player-id/
@@ -12,25 +12,169 @@ library(tidyverse)
 # devtools::install_github("BillPetti/baseballr") # before you run this code you need to update R
 library(baseballr)
 
+##### data
 
+cleanPlayerDraft <- read.csv("CleanPlayerDraft.csv")
 
+##### create scraping key for existing, undisputed results
 
+scrapingKeyClean <- cleanPlayerDraft %>% 
+  filter(!ID_missing) %>% 
+  filter(!Disputed) %>% 
+  select(Name, Pos, person_primary_position_abbreviation, person_id, RegLookup:ID_missing) %>% 
+  mutate(isPitcher = Pos %in% c("LHP", "RHP") | person_primary_position_abbreviation %in% c("P"),
+         person_id = as.character(person_id)) %>% 
+  select(Name, person_id:isPitcher)
 
+##### create scraping key for existing, disputed results
 
+# scrapingKeyDisputed <- cleanPlayerDraft %>%
+#   filter(Disputed)  %>% 
+#   select(Name, Pos, person_primary_position_abbreviation, person_id, RegLookup:ID_missing) %>% 
+#   mutate(isPitcher = Pos %in% c("LHP", "RHP") | person_primary_position_abbreviation %in% c("P"),
+#          person_id = as.character(person_id)) %>% 
+#   select(Name, person_id:isPitcher)
 
-# hu <- fg_batter_game_logs(playerid = 1902, year = 2004)
-# hu2 <- fg_milb_batter_game_logs(playerid = 1902, year = 2006)
-# fg_milb_pitcher_game_logs(playerid, year)
-# fg_pitcher_game_logs(playerid, year = 2017)
+# there are no disputed results in the current database
+
+##### create scraping key for missing results
+
+# scrapingKeyMissing <- cleanPlayerDraft %>% 
+#   filter(ID_missing)  %>% 
+#   select(Name, Pos, person_primary_position_abbreviation, person_id, RegLookup:ID_missing) %>% 
+#   mutate(isPitcher = Pos %in% c("LHP", "RHP") | person_primary_position_abbreviation %in% c("P"),
+#          person_id = as.character(person_id)) %>% 
+#   select(Name, person_id:isPitcher)
+
+##### create game logs for missing results
+
+# gameLogsMissing <- list()
 # 
-# lis <- list("minors_batting" = data.frame(1, 2, 3, 4),
-#             "majors_batting" = data.frame(1, 2, 3, 4),
-#             "majors_hitting" = data.frame(1, 2, 3, 4),
-#             "minors_hitting" = data.frame(1, 2, 3, 4))
+# for (i in 1:nrow(scrapingKeyMissing)) { # for each person in the scraping key
+#   
+#   if (scrapingKeyMissing$isPitcher[i]) { # for pitchers
+#     # make a list
+#     templist <- list(isPitcher = TRUE,
+#                      minors = data.frame(),
+#                      majors = data.frame())
+#     
+#   } else { # for hitters
+#     # make a list
+#     templist <- list(isPitcher = FALSE,
+#                      minors = data.frame(),
+#                      majors = data.frame())
+#   }
+#   
+#   # store the list
+#   gameLogsMissing[[scrapingKeyMissing$person_id[i]]] <- templist
+# }
 # 
-# l <- list("steve" = lis)
-# 
-# l[["joe"]] <- lis
-# 
-# l
+# saveRDS(gameLogsMissing, file = "GameLogsMissing.RDS")
+
+##### scrape game logs
+
+gameLogsClean <- list()
+
+for (i in 1:nrow(scrapingKeyClean)) {
+  
+  if (scrapingKeyClean$isPitcher[i]) { # for pitchers
+    
+    # make minors df
+    minors <- data.frame()
+    
+    # loop through all years they could have played
+    for (yr in 2005:2019) {
+      
+      # make temp
+      temp <- data.frame()
+      
+      # try function stops errors
+      try({
+        # pull down the game logs
+        temp <- fg_milb_pitcher_game_logs(playerid = scrapingKeyClean$RegLookup[i], year = yr)
+      }, silent = TRUE)
+      
+      # bind the game logs to the data frame
+      minors <- bind_rows(minors, temp)
+    }
+    
+    # make majors df
+    majors <- data.frame()
+    
+    # loop through all years they could have played
+    for (yr in 2005:2019) {
+      
+      # make temp
+      temp <- data.frame()
+      
+      # try function stops errors
+      try({
+        # pull down the game logs
+        temp <- fg_pitcher_game_logs(playerid = scrapingKeyClean$RegLookup[i], year = yr)
+      }, silent = TRUE)
+      
+      # bind the game logs to the data frame
+      majors <- bind_rows(majors, temp)
+    }
+    
+    # make the templist
+    templist <- list(isPitcher = TRUE,
+                     minors = minors,
+                     majors = majors)
+    
+  } else { # for hitters
+    
+    # make minors df
+    minors <- data.frame()
+    
+    # loop through all years they could have played
+    for (yr in 2005:2019) {
+      
+      # make temp
+      temp <- data.frame()
+      
+      # try function stops errors
+      try({
+        # pull down the game logs
+        temp <- fg_milb_batter_game_logs(playerid = scrapingKeyClean$RegLookup[i], year = yr)
+      }, silent = TRUE)
+      
+      # bind the game logs to the data frame
+      minors <- bind_rows(minors, temp)
+    }
+    
+    # make majors df
+    majors <- data.frame()
+    
+    # loop through all years they could have played
+    for (yr in 2005:2019) {
+      
+      # make temp
+      temp <- data.frame()
+      
+      # try function stops errors
+      try({
+        # pull down the game logs
+        temp <- fg_batter_game_logs(playerid = scrapingKeyClean$RegLookup[i], year = yr)
+      }, silent = TRUE)
+      
+      # bind the game logs to the data frame
+      majors <- bind_rows(majors, temp)
+    }
+    
+    # make the templist
+    templist <- list(isPitcher = FALSE,
+                     minors = minors,
+                     majors = majors)
+    
+  }
+  
+  # store the list
+  gameLogsClean[[scrapingKeyClean$person_id[i]]] <- templist
+  
+  # what iteration are you on
+  if (i %% 10 == 0) { cat("I am on:", i, "\n") }
+}
+
+saveRDS(gameLogsClean, file = "GameLogsClean.RDS")
 
--- test.R
+++ test.R
@@ -1,3 +1,3 @@
 # test file!
 
-
+# gameLogsMissing <- readRDS("GameLogsMissing.RDS")
